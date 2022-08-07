3e0f75374716c39f2ac78ff7814d5f3b85df061d
Sandy Maguire <sandy@sandymaguire.me>
2022-08-07 16:12:43 -0700

    chore: inline rectpacking

--- src/Search/Spatial.hs
+++ src/Search/Spatial.hs
@@ -1,29 +1,33 @@
-{-# LANGUAGE NumDecimals #-}
+{-# LANGUAGE NumDecimals     #-}
+{-# LANGUAGE RecordWildCards #-}
 
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Search.Spatial () where
 
 import           Control.Exception
+import           Control.Lens (view)
 import           Control.Monad.State (evalState, gets, modify, when)
 import           DB
 import           Data.Bool (bool)
 import           Data.Foldable (traverse_, asum)
 import           Data.Maybe (catMaybes, mapMaybe, fromMaybe)
-import           Data.QuadAreaTree (showTree)
-import           Data.RectPacking
+import           Data.Monoid
+import           Data.QuadAreaTree
 import qualified Data.Set as S
 import           Data.Text (Text)
 import qualified Data.Text as T
 import           Data.Traversable (for)
+import           Linear hiding (trace)
 import qualified Lucid as L
 import           Network.URI (uriPath)
-import           Rel8 hiding (bool, evaluate, max, index)
+import           Rel8 hiding (filter, bool, evaluate, max, index)
+import           Search.DoSearch (debugSearch)
 import           Search.Machinery
+import           Search.Spatial.Test.Data (testResults)
 import           Servant.Server.Generic ()
 import           Types
 import           Utils (unsafeURI)
-import Search.DoSearch (debugSearch)
 
 
 -- parameters:
@@ -37,15 +41,17 @@ instance SearchMethod 'Spatial where
   accumResults _ _ docs = do
     let best = maximum $ fmap sr_ranking docs
     let rs = fmap makeRect
-           $ fmap (\x -> x { sr_ranking = best - sr_ranking x }) docs
-    evaluate $ foldr place (makeTree (Region 0 0 150 30) Nothing) rs
+           $ fmap (\x -> x { sr_ranking = best - sr_ranking x })
+           $ filter (not . T.null . T.strip . sr_title) docs
+    evaluate $ foldr place (makeTree (Region 0 0 150 34) Nothing) rs
   showResults
     = traverse_ spaceResult
     . uniqueTiles
     . mapMaybe sequence
     . tile
     . fmap (fmap r_data)
-  debugResults = putStrLn . showTree (maybe '.' $ const 'X')
+  debugResults =
+    putStrLn . showTree (maybe ' ' $ toEnum . (+33) . view _x . r_size )
 
 
 
@@ -56,8 +62,8 @@ measureText s = V2 (T.length s + 1) 1
 
 makeRect :: SearchResult Identity -> Rect (SearchResult Identity)
 makeRect sr = Rect
-  { r_pos = V2 ((* 5) $ log $ max 1 $ fromIntegral $ fromMaybe 1e7 $ sr_popularity sr)
-               (sr_ranking sr * 3)
+  { r_pos = V2 ((* 1.4) $ log $ max 1 $ fromIntegral $ fromMaybe 1e6 $ sr_popularity sr)
+               (sr_ranking sr * 5)
   , r_size = measureText title'
   , r_data = sr { sr_title = title' }
   }
@@ -134,4 +140,65 @@ spaceResult (Region x y _ _, d) = do
 
 main :: IO ()
 main = debugSearch @'Spatial (Term "city") 1
+
+
+data Rect a = Rect
+  { r_pos    :: !(V2 Float)
+  , r_size   :: !(V2 Int)
+  , r_data   :: !a
+  }
+  deriving (Eq, Ord, Show, Functor)
+
+
+rectCenter :: Rect a -> V2 Float
+rectCenter Rect{..} = r_pos + fmap fromIntegral r_size / 2
+
+
+------------------------------------------------------------------------------
+-- | Take a size and a centerpoint, get the top left corner
+uncenter :: V2 Int -> V2 Float -> V2 Float
+uncenter sz center = center - fmap fromIntegral sz / 2
+
+
+place :: Eq a => Rect a -> QuadTree (Maybe (Rect a)) -> QuadTree (Maybe (Rect a))
+place r0 q0
+  | not $ inBounds q0 $ rectToRegion r0 = q0
+  | otherwise = go (0 :: Int) r0 q0
+  where
+    go 15 _ qt = qt
+    go n r qt =
+      case getFirst $ hitTest First (rectToRegion r) qt of
+        Nothing -> fillRect r qt
+        Just re ->
+          go (n + 1) (offsetBy r re) qt
+
+
+rectToRegion :: Rect a -> Region
+rectToRegion Rect{r_pos = fmap round -> V2 x y, r_size = V2 w h } = Region x y w h
+
+
+fillRect :: Rect a -> QuadTree (Maybe (Rect a)) -> QuadTree (Maybe (Rect a))
+fillRect r = fill (Just r) $ rectToRegion r
+
+
+------------------------------------------------------------------------------
+-- | Get the length of a vector from the center point  to the end of the size
+-- envelope
+extent :: V2 Float -> V2 Int -> Float
+extent dir (fmap fromIntegral -> V2 w h) =
+  norm $ V2 (dot dir $ V2 (w / 2) 0) (dot dir $ V2 0 (h / 2))
+
+
+offsetBy :: Rect a -> Rect a -> Rect a
+offsetBy want collide =
+  let dir = unzero $ normalize $ rectCenter want - rectCenter collide
+      get_ext = extent dir . r_size
+      new_center = rectCenter want + dir ^* ((get_ext want + get_ext collide) * 1.5)
+      res = want { r_pos = uncenter (r_size want) new_center }
+   in -- trace (show ("offsetting ", r_pos want, " to ", r_pos res, dir ))
+      res
+
+unzero :: V2 Float -> V2 Float
+unzero v | quadrance v < 0.1  = V2 0.707 0.707
+unzero v = v
 
